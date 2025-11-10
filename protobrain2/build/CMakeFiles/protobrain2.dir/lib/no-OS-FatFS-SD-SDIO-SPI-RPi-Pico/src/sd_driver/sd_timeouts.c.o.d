3108c9f8e5cad4760242449410a8a5316eb435eb
achan1989 <achan1989@gmail.com>
2025-11-10 20:28:27 +0000

    Remove the build directory
    It should be ephemeral

--- protobrain2/build/CMakeFiles/protobrain2.dir/lib/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/sd_driver/sd_timeouts.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/protobrain2.dir/lib/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/sd_driver/sd_timeouts.c.o: \
- /home/flyingthings/flur/flubogenV2-main/flubogenV2-main/FlubogenV2-main/FlubogenV2-main/protobrain2/lib/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/sd_driver/sd_timeouts.c \
- /home/flyingthings/flur/flubogenV2-main/flubogenV2-main/FlubogenV2-main/FlubogenV2-main/protobrain2/lib/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/include/sd_timeouts.h \
- /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h
--- protobrain2/build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/flyingthings/flur/flubogenV2-main/flubogenV2-main/FlubogenV2-main/FlubogenV2-main/protobrain2/build/_deps/picotool/"
-};
