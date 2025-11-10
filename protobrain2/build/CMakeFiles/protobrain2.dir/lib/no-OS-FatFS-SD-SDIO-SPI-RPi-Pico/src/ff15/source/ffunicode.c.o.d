0b8744c2a64370260ec8b3068889d812ef5787f7
achan1989 <achan1989@gmail.com>
2025-11-10 20:28:27 +0000

    Remove the build directory
    It should be ephemeral

--- protobrain2/build/CMakeFiles/protobrain2.dir/lib/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/ff15/source/ffunicode.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/protobrain2.dir/lib/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/ff15/source/ffunicode.c.o: \
- /home/flyingthings/flur/flubogenV2-main/flubogenV2-main/FlubogenV2-main/FlubogenV2-main/protobrain2/lib/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/ff15/source/ffunicode.c \
- /home/flyingthings/flur/flubogenV2-main/flubogenV2-main/FlubogenV2-main/FlubogenV2-main/protobrain2/lib/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/ff15/source/ff.h \
- /home/flyingthings/flur/flubogenV2-main/flubogenV2-main/FlubogenV2-main/FlubogenV2-main/protobrain2/lib/no-OS-FatFS-SD-SDIO-SPI-RPi-Pico/src/include/ffconf.h \
- /usr/lib/gcc/arm-none-eabi/12.2.1/include/stdint.h
