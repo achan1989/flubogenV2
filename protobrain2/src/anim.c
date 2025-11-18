bc729653c23022fd46a4cb9d145517cb51b7fc2e
achan1989 <achan1989@gmail.com>
2025-11-18 18:14:17 +0000

    Limit animation brightness to 150

--- protobrain2/src/anim.c
+++ protobrain2/src/anim.c
@@ -24,6 +24,42 @@ const char *ANIMATION_NAME[] = {
 	"BOOT ANIMATION"
 };
 
+/* TODO: temporary hack; just limits brightness to 150, this isn't real gamma correction. */
+static const uint8_t gammaCorrection[256] = {
+	0, 1, 1, 2, 2, 3, 4, 4,
+	5, 5, 6, 6, 7, 8, 8, 9,
+	9, 10, 11, 11, 12, 12, 13, 14,
+	14, 15, 15, 16, 16, 17, 18, 18,
+	19, 19, 20, 21, 21, 22, 22, 23,
+	24, 24, 25, 25, 26, 26, 27, 28,
+	28, 29, 29, 30, 31, 31, 32, 32,
+	33, 34, 34, 35, 35, 36, 36, 37,
+	38, 38, 39, 39, 40, 41, 41, 42,
+	42, 43, 44, 44, 45, 45, 46, 46,
+	47, 48, 48, 49, 49, 50, 51, 51,
+	52, 52, 53, 54, 54, 55, 55, 56,
+	56, 57, 58, 58, 59, 59, 60, 61,
+	61, 62, 62, 63, 64, 64, 65, 65,
+	66, 66, 67, 68, 68, 69, 69, 70,
+	71, 71, 72, 72, 73, 74, 74, 75,
+	75, 76, 76, 77, 78, 78, 79, 79,
+	80, 81, 81, 82, 82, 83, 84, 84,
+	85, 85, 86, 86, 87, 88, 88, 89,
+	89, 90, 91, 91, 92, 92, 93, 94,
+	94, 95, 95, 96, 96, 97, 98, 98,
+	99, 99, 100, 101, 101, 102, 102, 103,
+	104, 104, 105, 105, 106, 106, 107, 108,
+	108, 109, 109, 110, 111, 111, 112, 112,
+	113, 114, 114, 115, 115, 116, 116, 117,
+	118, 118, 119, 119, 120, 121, 121, 122,
+	122, 123, 124, 124, 125, 125, 126, 126,
+	127, 128, 128, 129, 129, 130, 131, 131,
+	132, 132, 133, 134, 134, 135, 135, 136,
+	136, 137, 138, 138, 139, 139, 140, 141,
+	141, 142, 142, 143, 144, 144, 145, 145,
+	146, 146, 147, 148, 148, 149, 149, 150,
+};
+
 #define FILE_NAME "file.fur"
 
 /* .fur file handle */
@@ -203,10 +239,7 @@ void startAnimation(uint8_t animationNumber) {
 			f_read(&filAnimation, &colord[0], 3, &br);
 
 			for (uint8_t j = 0; j < 3; j++) {
-				#warning unsafe! gamma needed for power limiting
-				/* TODO: restore the gamma correction functionality. */
-				//colorTable[i].raw[j] = gammaCorrection[colord[j]];
-				colorTable[i].raw[j] = colord[j];
+				colorTable[i].raw[j] = gammaCorrection[colord[j]];
 			}
 
 			printDebug("\tColor %u: R=%u G=%u B=%u\n", i, colorTable[i].rgb.r, colorTable[i].rgb.g, colorTable[i].rgb.b);
@@ -257,14 +290,11 @@ void updateAnimation() {
 		uint8_t color[3];
 		for (uint16_t i = 0; i < NUM_LEDS_FACE; i++) {
     		f_read(&filAnimation, &color, 3, &br);
-			#warning unsafe! gamma needed for power limiting
-			/* TODO: restore the gamma correction functionality. */
-			//colorBufferFace[i].raw[j] = gammaCorrection[color[j]];
 
 			/* The color is stored in the file as RGB bytes. */
-			colorBufferFace[i].r = color[0];
-			colorBufferFace[i].g = color[1];
-			colorBufferFace[i].b = color[2];
+			colorBufferFace[i].r = gammaCorrection[color[0]];
+			colorBufferFace[i].g = gammaCorrection[color[1]];
+			colorBufferFace[i].b = gammaCorrection[color[2]];
 		}
 
 		leds_write_buffer_to_channel(LED_CHANNEL_FACE);
