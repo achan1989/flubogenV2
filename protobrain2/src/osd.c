8d420f92feb3309e8a0888a0e47d3c1f2a1980e8
achan1989 <achan1989@gmail.com>
2025-11-30 17:26:11 +0000

    Tidy of osd module
    Put extra code into the init function.
    Renaming, making private, delete dead code.
    Formatting.

--- protobrain2/src/osd.c
+++ protobrain2/src/osd.c
@@ -1,136 +1,163 @@
-#include "main.h"
+#include <stdint.h>
+
+#include <hardware/gpio.h>
+#include <hardware/spi.h>
+
 #include "osd.h"
 
-const uint8_t defaultCodePage[128] = {
-		0,    0,    0,    0,    0,    0,    0,    0,
-		0,    0,    0,    0,    0,    0,    0,    0,
-		0,    0,    0,    0,    0,    0,    0,    0,
-		0,    0,    0,    0,    0,    0,    0,    0,
-	0x00,    0, 0x48,    0,    0,    0,    0, 0x46,
-	0x3F, 0x40,    0,    0, 0x45, 0x49, 0x41, 0x47,
-	0x0A, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-	0x08, 0x09, 0x44, 0x43, 0x4A,    0, 0x4B, 0x42,
-	0x4C, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11,
-	0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19,
-	0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20, 0x21,
-	0x22, 0x23, 0x24,    0,    0,    0,    0,    0,
-		0, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B,
-	0x2C, 0x2D, 0x2E, 0x2F, 0x30, 0x31, 0x32, 0x33,
-	0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B,
-	0x3C, 0x3D, 0x3E,    0,    0,    0,    0,    0
-};
+#define GPIO_PIN_OSD_CIPO 16
+#define GPIO_PIN_OSD_NCS 17
+#define GPIO_PIN_OSD_SCK 18
+#define GPIO_PIN_OSD_COPI 19
 
-uint16_t osd_buffer[480];	// 30 chars * 16 rows
+#define MAX7456_READ 0x80
+#define MAX7456_VM0 0x00
+#define MAX7456_VM1 0x01
+#define MAX7456_HOS 0x02
+#define MAX7456_VOS 0x03
+#define MAX7456_DMM 0x04
+#define MAX7456_DMAH 0x05
+#define MAX7456_DMAL 0x06
+#define MAX7456_DMDI 0x07
+#define MAX7456_CMM 0x08
+#define MAX7456_CMAH 0x09
+#define MAX7456_CMAL 0x0a
+#define MAX7456_CMDI 0x0b
+#define MAX7456_OSDM 0x0c
+#define MAX7456_RB0 0x10
+#define MAX7456_RB1 0x11
+#define MAX7456_RB2 0x12
+#define MAX7456_RB3 0x13
+#define MAX7456_RB4 0x14
+#define MAX7456_RB5 0x15
+#define MAX7456_RB6 0x16
+#define MAX7456_RB7 0x17
+#define MAX7456_RB8 0x18
+#define MAX7456_RB9 0x19
+#define MAX7456_RB10 0x1a
+#define MAX7456_RB11 0x1b
+#define MAX7456_RB12 0x1c
+#define MAX7456_RB13 0x1d
+#define MAX7456_RB14 0x1e
+#define MAX7456_RB15 0x1f
+#define MAX7456_OSDBL 0x6c
+#define MAX7456_STAT 0xA0
+#define MAX7456_CMDO 0xC0
+#define MAX7456_PAL_ROWS 16
 
-void busWrite(uint8_t address, uint8_t data) {
-	spi_get_hw(spi0)->dr = (uint32_t)((address << 8) | data);
-	while(spi_is_busy(spi0)) {};
+static const uint8_t defaultCodePage[128] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x48, 0x00, 0x00, 0x00, 0x00, 0x46,
+    0x3F, 0x40, 0x00, 0x00, 0x45, 0x49, 0x41, 0x47,
+    0x0A, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+    0x08, 0x09, 0x44, 0x43, 0x4A, 0x00, 0x4B, 0x42,
+    0x4C, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11,
+    0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19,
+    0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20, 0x21,
+    0x22, 0x23, 0x24, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B,
+    0x2C, 0x2D, 0x2E, 0x2F, 0x30, 0x31, 0x32, 0x33,
+    0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B,
+    0x3C, 0x3D, 0x3E, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+static uint16_t osd_buffer[480]; // 30 chars * 16 rows
+
+static void busWrite(uint8_t address, uint8_t data)
+{
+    spi_get_hw(spi0)->dr = (uint32_t)((address << 8) | data);
+    while (spi_is_busy(spi0))
+    {
+    };
 
     while (spi_is_readable(spi0))
         (void)spi_get_hw(spi0)->dr;
 }
 
-uint16_t busRead(uint8_t address) {
-	spi_get_hw(spi0)->dr = (address | 0x80) << 8;
-	while(spi_is_busy(spi0)) {};
+static uint16_t busRead(uint8_t address)
+{
+    spi_get_hw(spi0)->dr = (address | 0x80) << 8;
+    while (spi_is_busy(spi0))
+    {
+    };
 
-	return (uint16_t)spi_get_hw(spi0)->dr;
+    return (uint16_t)spi_get_hw(spi0)->dr;
 }
 
-void osd_print(uint8_t x, uint8_t y, char const * str, uint8_t attr) {
-	char c;
-	uint16_t addr;
+static void osd_print(uint8_t x, uint8_t y, char const *str, uint8_t attr)
+{
+    char c;
+    uint16_t addr;
 
-	addr = y * 30 + x;
-	attr = (attr & 3) << 5;
+    addr = y * 30 + x;
+    attr = (attr & 3) << 5;
 
-	while((c = *str++)) {
-		c = defaultCodePage[c & 127];
-		busWrite(MAX7456_DMAH, addr >> 8);
-		busWrite(MAX7456_DMAL, addr);
-		busWrite(MAX7456_DMDI, c);
-		busWrite(MAX7456_DMAH, (addr >> 8) + 2);
-		busWrite(MAX7456_DMAL, addr);
-		busWrite(MAX7456_DMDI, attr);
-		addr++;
-	}
+    while ((c = *str++))
+    {
+        c = defaultCodePage[c & 127];
+        busWrite(MAX7456_DMAH, addr >> 8);
+        busWrite(MAX7456_DMAL, addr);
+        busWrite(MAX7456_DMDI, c);
+        busWrite(MAX7456_DMAH, (addr >> 8) + 2);
+        busWrite(MAX7456_DMAL, addr);
+        busWrite(MAX7456_DMDI, attr);
+        addr++;
+    }
 }
-void init_osd() {
-	//spi_write_blocking(spi0, )
+
+void osd_init(void)
+{
+    // Init SPI0 for MAX7456
+    gpio_set_function(GPIO_PIN_OSD_CIPO, GPIO_FUNC_SPI);
+    gpio_set_function(GPIO_PIN_OSD_NCS, GPIO_FUNC_SPI);
+    gpio_set_function(GPIO_PIN_OSD_SCK, GPIO_FUNC_SPI);
+    gpio_set_function(GPIO_PIN_OSD_COPI, GPIO_FUNC_SPI);
+    spi_init(spi0, 5 * 1000 * 1000); // 50M / 10 = 5MHz, MAX7456 supports up to 10MHz
+    spi_set_format(spi0, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
+
+    // spi_write_blocking(spi0, )
 
     // Wait for POR done
-    //while(busRead(MAX7456_STAT) & 0x40);
+    // while(busRead(MAX7456_STAT) & 0x40);
 
     // Force software reset
     busWrite(MAX7456_VM0, 0x42);
 
     // Set screen buffer to all blanks
-    for (uint32_t c = 0; c < 480; c++) {
+    for (uint32_t c = 0; c < 480; c++)
+    {
         osd_buffer[c] = 0;
     }
 
     // Wait for software reset done
-    while(busRead(MAX7456_VM0) & 0x02);
+    while (busRead(MAX7456_VM0) & 0x02)
+    {
+    };
 
     // busWrite(MAX7456_VM0, 0b00100000);	// NTSC external sync, output off
-	busWrite(MAX7456_VM0, 0b00110000); // 0b01001000 PAL auto sync, OSD on ---- orginal: 0x48
+    busWrite(MAX7456_VM0, 0b00110000); // 0b01001000 PAL auto sync, OSD on ---- orginal: 0x48
     // Set all rows to same black/white level
-    for (uint32_t c = 0; c < 16; c++) {
-        busWrite(MAX7456_RB0 + c, 0b00000001);	// 0% black, 100% white
-		// busWrite(MAX7456_RB0 + c, 0b00001010);	// 0% black, 100% white
+    for (uint32_t c = 0; c < 16; c++)
+    {
+        busWrite(MAX7456_RB0 + c, 0b00000001); // 0% black, 100% white
+                                               // busWrite(MAX7456_RB0 + c, 0b00001010);	// 0% black, 100% white
     }
-	//busWrite(MAX7456_VM1, 0b11011100);	// Background 35% grey, 133ms blinking 50%
-    busWrite(MAX7456_VM1, 0b01001000);	// Background 0% grey, 133ms blinking 50%
+    // busWrite(MAX7456_VM1, 0b11011100);	// Background 35% grey, 133ms blinking 50%
+    busWrite(MAX7456_VM1, 0b01001000); // Background 0% grey, 133ms blinking 50%
 
-    busWrite(MAX7456_DMM, 0b01000000);	// Unique attributes mode, no auto-inc
-	uint8_t factory_OSDBL = (uint8_t) busRead(MAX7456_OSDBL); // must read OSDBL first and rewrite factory values
+    busWrite(MAX7456_DMM, 0b01000000);                       // Unique attributes mode, no auto-inc
+    uint8_t factory_OSDBL = (uint8_t)busRead(MAX7456_OSDBL); // must read OSDBL first and rewrite factory values
     busWrite(MAX7456_OSDBL, factory_OSDBL & 0b11101111);
-	//osd_print(12, 5, str_a, 3);
+    // osd_print(12, 5, str_a, 3);
 
-	// Display memory 0~511 = char, 512~1023 = attribute
-	// Write to DMDI
-	// Attribute byte: LBC, BLK, INV, 00000
+    // Display memory 0~511 = char, 512~1023 = attribute
+    // Write to DMDI
+    // Attribute byte: LBC, BLK, INV, 00000
 
-    busWrite(MAX7456_VM0, 0b01001000);	// 0b01001000 PAL auto sync, OSD on ---- 0x48
+    busWrite(MAX7456_VM0, 0b01001000); // 0b01001000 PAL auto sync, OSD on ---- 0x48
 }
-// void init_osd() {
-// 	//spi_write_blocking(spi0, )
-
-//     // Wait for POR done
-//     //while(busRead(MAX7456_STAT) & 0x40);
-
-//     // Force software reset
-//     busWrite(MAX7456_VM0, 0x42);
-
-//     // Set screen buffer to all blanks
-//     for (uint32_t c = 0; c < 480; c++) {
-//         osd_buffer[c] = 0;
-//     }
-
-//     // Wait for software reset done
-//     while(busRead(MAX7456_VM0) & 0x02);
-
-//     // busWrite(MAX7456_VM0, 0b00100000);	// NTSC external sync, output off
-// 	busWrite(MAX7456_VM0, 0b01101000); // 0b01001000 PAL auto sync, OSD on ---- orginal: 0x48
-//     // Set all rows to same black/white level
-//     for (uint32_t c = 0; c < 16; c++) {
-//         busWrite(MAX7456_RB0 + c, 0b00000001);	// 0% black, 100% white
-// 		// busWrite(MAX7456_RB0 + c, 0b00001010);	// 0% black, 100% white
-//     }
-// 	//busWrite(MAX7456_VM1, 0b11011100);	// Background 35% grey, 133ms blinking 50%
-//     busWrite(MAX7456_VM1, 0b10001100);	// Background 0% grey, 133ms blinking 50%
-
-//     busWrite(MAX7456_DMM, 0b01000000);	// Unique attributes mode, no auto-inc
-// 	uint8_t factory_OSDBL = (uint8_t) busRead(MAX7456_OSDBL); // must read OSDBL first and rewrite factory values
-//     busWrite(MAX7456_OSDBL, factory_OSDBL & 0b11101111);
-// 	//osd_print(12, 5, str_a, 3);
-
-// 	// Display memory 0~511 = char, 512~1023 = attribute
-// 	// Write to DMDI
-// 	// Attribute byte: LBC, BLK, INV, 00000
-
-//     busWrite(MAX7456_VM0, 0b01101000);	// 0b01001000 PAL auto sync, OSD on ---- 0x48
-// }
 
 /*void updateOSD(uint8_t animationNumber) {
 	// For some reason the OSD can only display 28 characters on one line instead of the 30 that is specified in datasheet
@@ -189,6 +216,6 @@ void init_osd() {
 	lineBuffer[sizeof(lineBuffer) - 1] = '\0';
 
 	max7456Write(1, MAX7456_LINES_NTSC - 2, lineBuffer, 0);
-	
+
 	max7456Update();
 }*/
