bffb805c409975fe766c18720af0b3c884276c0b
achan1989 <achan1989@gmail.com>
2026-09-05 20:02:52 +0100

    Support separate HTML files for different tabs
    Unfortunately this means they need to be loaded by XMLHttpRequest.
    Worth it for the maintainability win.

--- animator2/scripts/main.js
+++ animator2/scripts/main.js
@@ -20,7 +20,7 @@ function onNavTabClicked(event, clickedTab) {
   // Hide all tab contents except the one we want.
   const tabContents = document.getElementById("tabContentWrapper").getElementsByClassName("tabContent");
   for (const tabContentNode of tabContents) {
-    if (tabContentNode.id === clickedTab.name) {
+    if (tabContentNode.id === "tabContent_" + clickedTab.name) {
       tabContentNode.classList.add("active");
     } else {
       tabContentNode.classList.remove("active");
@@ -28,5 +28,26 @@ function onNavTabClicked(event, clickedTab) {
   }
 }
 
+function loadTabContents() {
+  const tabs = document.querySelectorAll("#navTabBar button.tab");
+  for (const tab of tabs) {
+    const name = tab.name;
+    if (name === "intro") continue;
+    const destination = document.querySelector("#tabContent_" + name);
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", (evt) => {
+      destination.replaceChildren(...xhr.response.body.childNodes)
+    });
+    xhr.addEventListener("error", (evt) => {
+      destination.innerHTML = "<p>Loading failed!</p>";
+    });
+    xhr.open("GET", "matrices.html");
+    xhr.responseType = "document";
+    xhr.send();
+  }
+}
+
 
 makeNavTabsClickable();
+loadTabContents();
